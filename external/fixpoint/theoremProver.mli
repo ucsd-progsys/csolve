4b11d591bbdcebca0a8b2e32efb4beae33b6e305
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2009-04-10 17:43:39 -0700

    add z3 and base code for fixpoint

---
+++ external/fixpoint/theoremProver.mli
@@ -0,0 +1,7 @@
+val push_axiom : Sort.t Symbol.SMap.t -> pred -> unit
+
+val set_and_filter : Sort.t Symbol.SMap.t -> pred list -> pred list -> pred list
+
+val print_stats : unit -> unit
+
+val not_reset : unit -> unit
---
+++ external/z3/ocaml/clean.sh
@@ -0,0 +1,6 @@
+rm -f *.a
+rm -f *.cmx*
+rm -f *.cmi
+rm -f *.dll
+rm -f *.o
+rm -f *.lib
