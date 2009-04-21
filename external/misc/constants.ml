a120b6ae019160e7bb5b378824d915483e219227
Ranjit Jhala <rjhala@pho.(none)>
2009-04-20 22:28:42 -0700

    random changes

---
+++ external/misc/constants.ml
@@ -0,0 +1,6 @@
+
+
+let ol_solve = failwith "TBD: ol_solve"
+let ck_olev _ = failwith "TBD: Constants.ck_olev"
+let no_simple = failwith "TBD: no_simple"
+let verify_simple = failwith "TBD: no_simple"
