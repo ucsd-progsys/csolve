0062b5064005b8471e1a1f49a6c9e23032d15f80
Pat Rondon <pmr@famine>
2009-05-10 16:54:42 -0700

    Add various tests for the store inference

---
+++ tests/test12.c
@@ -0,0 +1,8 @@
+void main () {
+    int x;
+
+    x = 0;
+    while (1) {
+        x = x + 4;
+    }
+}
---
+++ tests/test18.c
@@ -0,0 +1,10 @@
+void main() {
+    int i;
+
+    i = 1;
+    while (1) {
+        i = i * 2;
+    }
+
+    return;
+}
