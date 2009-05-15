ed3eeb5cfbd1ff1a35b86da7dc973b9fa840b640
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2009-05-15 15:26:47 -0700

    annotation interface for pointers

---
+++ src/refanno.mli
@@ -0,0 +1,11 @@
+type cloc
+
+type annotation =
+  RefGen of Ctypes.sloc | RefInst of cloc * Ctypes.sloc
+
+(* tuple denotes (before, after) instruction *) 
+type annotated_block = (annotation option * annotation option) list
+
+(* takes an instruction list and returns a list of annotations of identical
+ * size *)
+val annotate_block: Cil.instr list -> (annotated_block * (Cil.varinfo -> cloc))
