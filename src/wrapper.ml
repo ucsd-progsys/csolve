e4db077a18107ae7d3560098cdb1e3a5c1e26391
Ranjit Jhala <rjhala@pho.(none)>
2009-05-18 17:22:59 -0700

    refactoring cilenv/cilreft/name -- changes to wrapper.mli (and .ml)

--- src/wrapper.ml
+++ src/wrapper.ml
@@ -21,56 +21,55 @@ open Cil
 (*******************************************************************)
 (************************** Environments ***************************)
 (*******************************************************************)
-type cilname = Sy.t
-let cilname_of_varinfo = fun v -> Sy.of_string v.vname
-let cilname_of_string = Sy.of_string
+type name = Sy.t
+let name_of_varinfo = fun v -> Sy.of_string v.vname
+let name_of_string = Sy.of_string
 
 type cilreft = Base of C.reft 
-             | Fun  of (cilname * cilreft) list * cilreft  
+             | Fun  of (name * cilreft) list * cilreft  
 
 type cilenv  = cilreft YM.t
 
 let ce_empty = 
   YM.empty
 
-let ce_mem v cenv = 
-  YM.mem (Sy.of_string v.vname) cenv
+let ce_mem n cenv = 
+  YM.mem n cenv
 
-let ce_find v (cenv : cilreft YM.t) =
-  try YM.find (Sy.of_string v.vname) cenv with Not_found -> 
-    assertf "Unknown variable! %s" v.vname 
+let ce_find n (cenv : cilreft YM.t) =
+  try YM.find n cenv with Not_found -> 
+    assertf "Unknown name! %s" (Sy.to_string n)
 
-let ce_add v cr cenv = 
-  YM.add (cilname_of_varinfo v) cr cenv
+let ce_add n cr cenv = 
+  YM.add n cr cenv
 
-let ce_project base_env fun_env vs =
-  vs
-  |> Misc.map cilname_of_varinfo 
+let ce_project base_env fun_env ns =
+  ns
   |> Misc.filter (fun vn -> not (YM.mem vn base_env))
   |> List.fold_left begin 
-       fun env vn -> 
-         asserts (YM.mem vn fun_env) "ce_project";
-         YM.add vn (YM.find vn fun_env) env
+       fun env n -> 
+         asserts (YM.mem n fun_env) "ce_project";
+         YM.add n (YM.find n fun_env) env
      end base_env
 
-let ce_unroll v env =
-  match ce_find v env with
+let ce_unroll n env =
+  match ce_find n env with
   | Fun (vcrs, cr) -> 
       let env' = List.fold_left 
-                   (fun env (vn, cr) -> YM.add vn cr env) 
+                   (fun env (n', cr') -> YM.add n' cr' env) 
                    env vcrs in
       (env', cr)
   | _ ->
       assertf "ce_unroll"
 
 let ce_iter f cenv = 
-  YM.iter (fun vn cr -> f vn cr) cenv
+  YM.iter (fun n cr -> f n cr) cenv
 
 let env_of_cilenv cenv = 
   YM.fold begin
-    fun vn cr env -> 
+    fun n cr env -> 
       match cr with 
-      | Base r -> YM.add vn r env
+      | Base r -> YM.add n r env
       | _      -> env
   end cenv YM.empty
        
@@ -82,13 +81,13 @@ let rec print_cilreft so ppf = function
         (Misc.pprint_many false "," (print_binding so)) args
         (print_cilreft so) ret
 
-and print_binding so ppf (vn, cr) = 
-  F.fprintf ppf "%a : %a" Sy.print vn (print_cilreft so) cr
+and print_binding so ppf (n, cr) = 
+  F.fprintf ppf "%a : %a" Sy.print n (print_cilreft so) cr
 
 let print_ce so ppf cenv =
   YM.iter begin
-    fun vn cr -> 
-      F.fprintf ppf "@[%a@]@\n" (print_binding so) (vn, cr) 
+    fun n cr -> 
+      F.fprintf ppf "@[%a@]@\n" (print_binding so) (n, cr) 
   end cenv
 
 (*******************************************************************)
@@ -115,7 +114,7 @@ and cilreft_of_bindings f = function
       []
   | Some sts -> 
       List.map begin
-        fun (s,t,_) -> (cilname_of_string s, cilreft_of_type f t) 
+        fun (s,t,_) -> (name_of_string s, cilreft_of_type f t) 
       end sts
 
 let is_base = function
@@ -136,9 +135,21 @@ let t_exp env t e =
   let ras = [C.Conc (A.pAtom (A.eVar vv, A.Eq, e))] in
   Base (C.make_reft vv so ras)
 
+(*
 let t_var env v =
   asserts (ce_mem v env) "t_var: reading unbound var"; 
   t_exp env v.Cil.vtype (Lval ((Var v), NoOffset))
+*)
+
+let t_name env n = 
+  asserts (YM.mem n env) "t_cilname: reading unbound var";
+  match YM.find n env with
+  | Base r -> 
+      let so  = C.sort_of_reft r in
+      let vv  = Sy.value_variable so in
+      let ras = [C.Conc (A.pAtom (A.eVar vv, A.Eq, A.eVar n))] in
+      Base (C.make_reft vv so ras)
+  | cr -> cr
 
 (****************************************************************)
 (********************** Constraints *****************************)
@@ -159,59 +170,3 @@ let rec make_wfs env cr loc =
       let env' = List.fold_left (fun env (vn, cr) -> YM.add vn cr env) env args in
       (make_wfs env' ret loc) ++ 
       (Misc.flap (fun (_, cr) -> make_wfs env' cr loc) args)
-(*
-(****************************************************************)
-(********************** Constraint Indexing *********************)
-(****************************************************************)
-
-type t = {
-  scim : ST.ssaCfgInfo SM.t;
-  wfm  : C.wf list SM.t;
-  cm   : C.t list SM.t;
-}
-
-(* API *)
-let create_t ws cs = 
-  {scim = Misc.StringMap.empty;
-   wfm  = Misc.StringMap.add Constants.global_name ws Misc.StringMap.empty;
-   cm   = Misc.StringMap.add Constants.global_name cs Misc.StringMap.empty}
-
-(* API *)
-let add_t me fn sci wfs cs env =
-  { scim = SM.add fn sci me.scim ;
-    wfm  = SM.add fn wfs me.wfm ;
-    cm   = SM.add fn cs  me.cm ;
-  }
-
-let find_t me fn = 
-  (SM.find fn me.scim, 
-   SM.find fn me.wfm,
-   SM.find fn me.cm)
-
-let iter_t me f = 
-  SM.iter (fun fn _ -> f fn (find_t me fn)) me.scim
-
-(* API *)
-let print_t so ppf me = 
-  match so with 
-  | None -> (* print constraints *) 
-      iter_t me begin 
-        fun fn (_, wfs, cs) ->
-          F.fprintf ppf "Ref-Constraints for %s \n %a" 
-          fn (Misc.pprint_many true "\n" (C.print_t None)) cs;
-          F.fprintf ppf "WF-Constraints for %s \n %a"
-          fn (Misc.pprint_many true "\n" (C.print_wf None)) wfs
-      end
-  | Some _ -> (* print solution *)
-      iter_t me begin
-        fun fn (_, ws, _) ->
-          ws |> env_of_ws 
-             |> F.printf "Liquid Types for %s \n%a" fn (C.print_env so)
-      end
-
-(* API *)
-let wfs_of_t = fun me -> SM.fold (fun _ wfs acc -> wfs ++ acc) me.wfm []
-
-(* API *)
-let cs_of_t  = fun me -> SM.fold (fun _ cs acc -> cs ++ acc) me.cm []
-*)
