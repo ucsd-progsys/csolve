c1375e468889e0e1a1e897282974c068e1c2b959
Ranjit Jhala <rjhala@pho.(none)>
2009-05-19 20:34:44 -0700

    renaming files

--- src/refanno.mli
+++ src/refanno.mli
@@ -2,12 +2,12 @@ type cloc
 
 val cbot: cloc
 
-type annotation =
-  RefGen of Ctypes.sloc | RefInst of cloc * Ctypes.sloc
+type annotation = RefGen of Ctypes.sloc | RefInst of cloc * Ctypes.sloc
 
 (* tuple denotes (before, after) instruction *) 
 type annotated_block = (annotation option * annotation option) list
 
-(* takes an instruction list and returns a list of annotations of identical
- * size *)
+(* takes an instruction list and returns a list of annotations of identical size *)
 val annotate_block: Cil.instr list -> (annotated_block * (Cil.varinfo -> cloc))
+
+
