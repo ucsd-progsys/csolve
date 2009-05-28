57d052b508bf38153f11c4d4937badfdea182f0b
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2009-05-27 17:30:25 -0700

    asdfa

--- tests/test00p.c
+++ tests/test00p.c
@@ -1,16 +1,25 @@
+extern char* malloc(int);
+
 int main(){
-  int *x,*y,*t;
-  int z=1;
+  //int *x,*y;
+  int *r;
+  //int z=1;
+  
+  /*x = (int*)malloc(4);
+  y = (int*)malloc(4);
 
   *x = z;
-  z = *x;
+  //z = (int)*x;
+  z = (int) malloc(4);
   x = y;
   *y = z;
-  *x = z;
+  *x = z;*/
 
-  if (z > 0) { t=x; }
+  //if (z > 0) { t = x; }
+  //else { t = (int*)malloc (4); }
+  r = (int*) malloc(4);
 
-  *t = 5; 
+  //*r = 5; 
 
 //  assert(y >= 0);
 
