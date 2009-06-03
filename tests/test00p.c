ab0bcbb4d4278bcedf67edb3a0fa1c7705a40692
Ranjit Jhala <rjhala@pho.(none)>
2009-06-02 21:36:09 -0700

    random tweaks

--- tests/test00p.c
+++ tests/test00p.c
@@ -1,27 +1,7 @@
 extern char* malloc(int);
 
 int main(){
-  //int *x,*y;
-  int *r;
-  //int z=1;
-  
-  /*x = (int*)malloc(4);
-  y = (int*)malloc(4);
-
-  *x = z;
-  //z = (int)*x;
-  z = (int) malloc(4);
-  x = y;
-  *y = z;
-  *x = z;*/
-
-  //if (z > 0) { t = x; }
-  //else { t = (int*)malloc (4); }
-  r = (int*) malloc(4);
-
-  //*r = 5; 
-
-//  assert(y >= 0);
-
-  return 0;
+ int *r;
+ r = (int*) malloc(4);
+ return 0;
 }
