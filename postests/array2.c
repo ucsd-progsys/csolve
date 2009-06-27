70b8479abe956ed26d6892c372681a66c7debba7
Ranjit Jhala <jhala@chowder.ucsd.edu>
2009-06-26 19:41:04 -0700

    minor tweak to test

--- postests/array2.c
+++ postests/array2.c
@@ -2,10 +2,14 @@ extern char* malloc(int);
 
 void main(){
   int *x;
+  int *y;
+
   int i;
 
-  x = (int *) malloc(100);
+  y = (int *) malloc(100);
   
+  x = y;
+
   i = 0;
   
   for (; i < 100; i++){  
@@ -13,6 +17,7 @@ void main(){
     *x = 0;
     x++;
   }
-  
+ 
+
   return 0;
 }
