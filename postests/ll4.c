ddee9636f5eb39caeb8e5258232b46d7599de5a6
Ranjit Jhala <rjhala@pho.(none)>
2009-06-27 16:24:48 -0700

    twww

---
+++ postests/ll4.c
@@ -0,0 +1,27 @@
+extern char* malloc(int);
+
+typedef struct node {
+  int data;
+  struct node *next;
+} node_t;
+
+node_t *main(){
+  node_t *root;
+  node_t *tmp;
+  int i,n;
+
+  n = 100;
+
+  root = 0;
+
+  for(i = 0; i < n; i++){
+    tmp       = (node_t *) malloc(sizeof(node_t));
+    tmp->data = i;
+    tmp->next = root;
+    root      = tmp;
+  }
+
+//  assert(0); //SANITY
+
+  return root;
+}
