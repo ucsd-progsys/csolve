adef313938a26fbf2bf8789837f5588d527c52ca
Pat Rondon <pmr@meatwad.ucsd.edu>
2009-07-09 15:02:56 -0700

    Example from the paper goes through with some caveats and too many quals

---
+++ postests/stringexample.c
@@ -0,0 +1,92 @@
+extern char *malloc(int);
+
+char *make_string(int n) {
+    if (n < 0)
+        return 0;
+
+    char *str0 = (char *)malloc(n * sizeof(char));
+    char *str  = str0;
+
+    for (int i = 0; i < n; i++) {
+        validptr(str);
+        *str++ = 0;
+    }
+
+    return str0;
+}
+
+typedef struct {
+    int  len;
+    char *str;
+} string;
+
+void init_string(string *s, char c) {
+    int len;
+    char *str;
+
+    s = s; // THETA ISSUE
+
+    // pmr: weirdness here, these need to be rolled out...
+    len = s->len;
+    str = s->str;
+    for (int i = 0; i < len; i++) {
+        validptr(str + i);
+        s->str[i] = c;
+    }
+}
+
+string *new_string(int n, char c) {
+    string *s;
+    char   *str;
+    int tmp;
+
+    if (n < 0)
+        return 0;
+
+    str    = make_string(n);
+    s      = (string *)malloc(sizeof(string));
+    s->len = n;
+    str    = make_string(n);
+    s->str = str;
+
+    init_string(s, c);
+    // assert(0); // Sanity
+
+    return s;
+}
+
+typedef struct _slist {
+    struct _slist *next;
+    string        *s;
+} slist;
+
+slist *new_strings(int n) {
+    string *s;
+    slist  *sl, *t;
+    string *str;
+
+    sl = 0;
+    for (int i = 1; i < n; i++) {
+        s      = (string *)malloc(sizeof(string));
+        s->len = i;
+        str    = make_string(i); // sloc_of_ret ISSUE
+        s->str = str;
+
+        t       = (slist *)malloc(sizeof(slist));
+        t->s    = s;
+        t->next = sl;
+        sl      = t;
+    }
+
+    return sl;
+}
+
+void main () {
+    // pmr: unfortunately, this needs to be positive; figure out why
+    int n = nondetpos();
+
+    char *str = make_string(n);
+    new_string (nondetpos (), (char) nondetpos ());
+
+    slist *sls = new_strings(n);
+}
