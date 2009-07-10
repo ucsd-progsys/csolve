3e14ccd0a5b333bfb03745c30a6b2cff61120322
Andrey Rybalchenko <rybal@swsao-ar.vs.mpi-sws.org>
2009-07-11 01:23:31 +0200

    dottification

--- external/fixpoint/toDot.ml
+++ external/fixpoint/toDot.ml
@@ -1,11 +1,10 @@
 module C = FixConstraint
-module StrMap = Map.Make (struct type t = string let compare = compare end)
 module StrSet = Set.Make (struct type t = string let compare = compare end)
 open Misc.Ops
 
 
 module V = struct
-  type t = StrSet.t
+  type t = string
   let compare = Pervasives.compare
   let hash = Hashtbl.hash
   let equal = (=)
@@ -21,11 +20,7 @@ module G = Graph.Persistent.Digraph.ConcreteLabeled(V)(E)
 
 module Display = struct
   include G
-  let vertex_name v =
-    if StrSet.is_empty v then
-      "empty"
-    else
-      "\"" ^ String.escaped (StrSet.elements v |> String.concat ", ") ^ "\""
+  let vertex_name v = "\"" ^ String.escaped v ^ "\""
   let graph_attributes _ = []
   let default_vertex_attributes _ = []
   let vertex_attributes _ = []
@@ -36,27 +31,110 @@ end
 
 module DotOutput = Graph.Graphviz.Dot(Display)
 
+module SCC = Graph.Components.Make(G) 
 
-let t_to_edge t = 
+let vertices_of_graph g = G.fold_vertex (fun v vs -> v::vs) g []
+
+let set_of_strings = List.fold_left (fun s x -> StrSet.add x s) StrSet.empty
+
+let set_to_string default s = 
+  if StrSet.is_empty s then default else StrSet.elements s |> List.map String.escaped |> String.concat ", "
+
+let edges_to_graph es = List.fold_left (fun g e -> G.add_edge_e g e) G.empty es
+
+(* k_1, ..., k_n <: k_0 depends on l_1, ..., l_m <: l_0 iff l_0 = k_i for some 1 \leq i \leq n *)
+
+
+let t_to_dep t = 
   let env = C.env_of_t t in
-    (*   let grd = C.grd_of_t t in *)
   let lhs = C.lhs_of_t t in
   let rhs = C.rhs_of_t t in
   let tag = try string_of_int (C.id_of_t t) with _ -> 
     failure "ERROR: t_to_edge: anonymous constraint %s" (C.to_string t) in
-  let kvs_to_strset default kvs = 
-    if kvs = [] then 
-      StrSet.add default StrSet.empty 
-    else
-      List.fold_left 
-	(fun s kv -> StrSet.add (snd kv |> Ast.Symbol.to_string) s) StrSet.empty kvs in
   let src =
     C.kvars_of_reft lhs :: List.map (fun b -> snd b |> C.kvars_of_reft) (C.bindings_of_env env) |> 
-	List.flatten |> kvs_to_strset "start" in
-  let dst = C.kvars_of_reft rhs |> kvs_to_strset "error" in
-    G.E.create src tag dst
+	List.flatten |> List.map snd |> List.map Ast.Symbol.to_string |> set_of_strings in
+  let dst = C.kvars_of_reft rhs |> List.map snd |> List.map Ast.Symbol.to_string |> set_of_strings in
+    src, tag, dst
+
+let sccs_to_dot g prefix =
+  let n, scc_of = SCC.scc g in
+  let vs = vertices_of_graph g in
+    Printf.printf "%s #scc = %d\n" prefix n;
+    for i = 0 to n-1 do
+      let scc, rest = List.partition (fun v -> scc_of v = i) vs in
+      let g' = List.fold_left (fun g'' v -> G.remove_vertex g'' v) g rest in 
+      let out = open_out (Printf.sprintf "/tmp/%s-scc-%d.dot" prefix i) in
+	Printf.printf "%s scc %d %s\n" prefix i (String.concat ", " scc);
+	DotOutput.output_graph out g';
+	close_out out
+    done
+
+
+let mk_dep_graph ts =
+  let ds' = List.map t_to_dep ts in
+  let ds = List.map (fun (src, tag, dst) ->
+		       (if StrSet.is_empty src then StrSet.singleton "start" else src),
+		       tag,
+		       (if StrSet.is_empty dst then StrSet.singleton "error" else dst)
+		    ) ds' in
+  let g = 
+    List.map
+      (fun (src, tag, dst) ->
+	 Misc.map_partial
+	   (fun (src', tag', dst') ->
+	      let inter = StrSet.inter dst src' in
+		if StrSet.is_empty inter then 
+		  None
+		else 
+		  Some(G.E.create tag (set_to_string "" inter) tag') (* tag depends on tag' via inter   *)
+	   ) ds
+      ) ds |> List.flatten |> edges_to_graph in
+  let oc = open_out "/tmp/dep.dot" in
+    DotOutput.output_graph oc g;
+    close_out oc;
+    sccs_to_dot g "dep"
+
+
+let other_graph ts =
+  let deps = List.map t_to_dep ts in
+  let srcs, dsts = List.map (fun (s, _, d) -> s, d) deps |> List.split in
+  let es = List.map (fun (src, tag, dst) ->
+		       G.E.create (set_to_string "start" src) tag (set_to_string "error" dst) 
+		    ) deps in
+  let es' = List.fold_left (fun es'' dst ->
+			      Misc.map_partial (fun src -> 
+						  if StrSet.diff dst src |> StrSet.is_empty then
+						    Some (G.E.create (set_to_string "error" dst) "" (set_to_string "start" src))
+						  else
+						    None
+					       ) srcs ++ es''
+			   ) es dsts in
+  let g = List.fold_left (fun g e -> G.add_edge_e g e) G.empty es' in
+    g
+
+let t_to_edge t = 
+  let srcs', tag, dsts' = t_to_dep t in
+  let srcs = if StrSet.is_empty srcs' then ["start"] else StrSet.elements srcs' in
+  let dsts = if StrSet.is_empty dsts' then ["error"] else StrSet.elements dsts' in
+    List.fold_left (fun es src -> List.map (G.E.create src tag) dsts ++ es) [] srcs
+
+
+    
 
 let to_dot oc ts =
-  let g = 
-    List.fold_left (fun g t -> t_to_edge t |> G.add_edge_e g) G.empty  ts in
-    DotOutput.output_graph oc g
+  let _ =  List.fold_left (fun g e -> G.add_edge_e g e ) G.empty (List.map t_to_edge ts |> List.flatten) in
+  let g = other_graph ts in
+  let vs = G.fold_vertex (fun v vs' -> v::vs') g [] in
+  let n, scc_of = SCC.scc g in
+    DotOutput.output_graph oc g;
+    Printf.printf "#scc = %d\n" n;
+    for i = 0 to n-1 do
+      let scc, rest = List.partition (fun v -> scc_of v = i) vs in
+      let g' = List.fold_left (fun g'' v -> G.remove_vertex g'' v) g rest in 
+      let out = open_out (Printf.sprintf "/tmp/scc-%d.dot" i) in
+	Printf.printf "scc %d %s\n" i (String.concat ", " scc);
+	DotOutput.output_graph out g';
+	close_out out
+    done;
+    mk_dep_graph ts
