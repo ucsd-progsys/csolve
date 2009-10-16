f79585d6167629e374e95656c6d5d6c76772ef7c
Ranjit Jhala <rjhala@pho.(none)>
2009-10-16 13:51:49 -0700

    fiddling with arraylist2.c, adding unsoundness regrtest arraylist3-unsafe.c

--- negtests/arraylist3-unsafe.c
+++ negtests/arraylist3-unsafe.c
@@ -2,35 +2,28 @@ extern char* malloc(int);
 
 struct foo {
   int data;
-  struct foo *next;
 };
 
 struct foo *__attribute__((array)) main(){
   struct foo *a;
-  struct foo *vt;
   struct foo *b;
 
-  int i;
+  int i,j;
   int n;
 
   n  = nondetpos();
   a  = (struct foo *) malloc(100 * sizeof(struct foo));
-  vt = (struct foo *) 0;
-  
+ 
   i = n-1;
   while (i >= 0){
     b 	    = a + i;
-    b->data = 99999;
-    b->next = vt;
-    //vt = b;
+    b->data = 999;
     i--;
   }
 
-  for (i=0; i < n; i++){
-    b = a[i].next;
-    if (b != 0){
-      validptr(b);
-    }
+  for (j=0; j < 5; j++){
+    b       = a + j;
+    assert(b->data == 999);
   }
 
   return a;
