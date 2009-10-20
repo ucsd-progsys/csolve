e37659eabe95b5bb16e04dd0c281b8ce5c33ac1a
Ranjit Jhala <rjhala@pho.(none)>
2009-10-20 16:20:56 -0700

    random commit before refactoring cilInterface.mli

--- src/cilInterface.mli
+++ src/cilInterface.mli
@@ -26,7 +26,4 @@
 
 val expr_of_cilexp: (unit -> Ast.expr) -> Cil.exp -> Ast.expr
 val pred_of_cilexp: (unit -> Ast.expr) -> Cil.exp -> Ast.pred
-
-(* val expr_of_var: Cil.varinfo -> Ast.expr
-   val expr_of_lval: Cil.lval -> Ast.expr *)
-
+val reft_of_cilexp: 
