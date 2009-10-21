5cea07e02c10ddf65595bd66d92436c802e06aed
Pat Rondon <pmr@meatwad.ucsd.edu>
2009-10-21 13:59:42 -0700

    tweak

--- external/misc/misc.ml
+++ external/misc/misc.ml
@@ -39,6 +39,8 @@ module Ops = struct
 
   let (>>) x f = f x; x
 
+  let (>>>) x f = ignore x; f
+
   let (|>>) xo f = match xo with None -> None | Some x -> f x
 
   let (+=) x n = x := !x + n; !x
--- src/inferctypes.ml
+++ src/inferctypes.ml
@@ -696,21 +696,24 @@ let rec solve_and_check (cf: cfun) (vars: ctype VM.t) (em: ctvemap) (bas: RA.blo
 let d_vartypes () vars =
   P.docList ~sep:(P.dprintf "@!") (fun (v, ct) -> P.dprintf "%s: %a" v.C.vname Ctypes.d_ctype ct) () vars
 
-let print_shape (fname: string) (cf: cfun) ({vtyps = locals; store = st}: shape) (ds: Ind.dcheck list): unit =
-  let _ = P.printf "%s@!" fname in
-  let _ = P.printf "============@!@!" in
-  let _ = P.printf "Signature:@!" in
-  let _ = P.printf "----------@!@!" in
-  let _ = P.printf "%a@!@!" d_cfun cf in
-  let _ = P.printf "Locals:@!" in
-  let _ = P.printf "-------@!@!" in
-  let _ = P.printf "%a@!@!" d_vartypes locals in
-  let _ = P.printf "Store:@!" in
-  let _ = P.printf "------@!@!" in
-  let _ = P.printf "%a@!@!" d_store st in
-  let _ = P.printf "Deferred Checks:@!" in
-  let _ = P.printf "------@!@!" in
-  let _ = P.printf "%a@!@!" (P.d_list "\n" Ind.d_dcheck) ds in
+let print_shape (fname: string) (cf: cfun) ({vtyps = locals; store = st; anna = annot}: shape) (ds: Ind.dcheck list): unit =
+  P.printf "%s@!" fname >>>
+  P.printf "============@!@!" >>>
+  P.printf "Signature:@!" >>>
+  P.printf "----------@!@!" >>>
+  P.printf "%a@!@!" d_cfun cf >>>
+  P.printf "Locals:@!" >>>
+  P.printf "-------@!@!" >>>
+  P.printf "%a@!@!" d_vartypes locals >>>
+  P.printf "Store:@!" >>>
+  P.printf "------@!@!" >>>
+  P.printf "%a@!@!" d_store st >>>
+  P.printf "Annotations:@!" >>>
+  P.printf "------@!@!" >>>
+  P.printf "%a@!@!" RA.d_block_annotation_array annot >>>
+  P.printf "Deferred Checks:@!" >>>
+  P.printf "------@!@!" >>>
+  P.printf "%a@!@!" (P.d_list "\n" Ind.d_dcheck) ds >>>
     ()
 
 let infer_shape (fe: funenv) (scim: Ssa_transform.ssaCfgInfo CilMisc.VarMap.t) (cf: cfun) (sci: ST.ssaCfgInfo): shape * Ind.dcheck list =
