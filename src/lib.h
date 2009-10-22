a0c2359dc2591d400cf50eee44792c7d152bb2d2
Pat Rondon <pmr@meatwad.ucsd.edu>
2009-10-22 12:19:27 -0700

    script to use in place of cc (cc.sh) for merging; dummy stdlib header (lib.h)

---
+++ src/lib.h
@@ -0,0 +1,12 @@
+/* Various useful definitions */
+
+extern void *malloc(int);
+extern void assert(int);
+
+#define NULL   0
+
+#define FILE   int
+
+#define stdout 0
+#define stdin  1
+#define stderr 2
