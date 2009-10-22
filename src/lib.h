a03a42a69b3116d27bcf58389f9023810a38f151
Pat Rondon <pmr@meatwad.ucsd.edu>
2009-10-22 12:28:03 -0700

    more useful prototypes

--- src/lib.h
+++ src/lib.h
@@ -1,7 +1,13 @@
 /* Various useful definitions */
 
 extern void *malloc(int);
+
 extern void assert(int);
+extern void exit(int);
+
+extern long int atol(char *);
+
+extern char *strtok(char *, char *);
 
 #define NULL   0
 
@@ -10,3 +16,9 @@ extern void assert(int);
 #define stdout 0
 #define stdin  1
 #define stderr 2
+
+extern FILE *fopen(char *, char *);
+extern int fprintf(FILE *, char *, ...);
+extern char *fgets(char *, int, FILE *);
+
+extern int sscanf(char *, char *, ...);
