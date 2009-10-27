e07747a8931f252d721eb64bb41775875a7a0a9e
Pat Rondon <pmr@meatwad.ucsd.edu>
2009-10-27 15:11:08 -0700

    shapeInfra: stop being such a sissy and fresh enums

--- src/shapeInfra.ml
+++ src/shapeInfra.ml
@@ -13,7 +13,8 @@ let fresh_heaptype (t: C.typ): ctype =
   let ats1 = typealias_attrs t in
     match C.unrollType t with
       | C.TInt (ik, _)                           -> CTInt (C.bytesSizeOfInt ik, ITop)
+      | C.TEnum (ei, _)                          -> CTInt (C.bytesSizeOfInt ei.C.ekind, ITop)
       | C.TFloat _                               -> CTInt (CM.typ_width t, ITop)
       | C.TVoid _                                -> void_ctype
       | C.TPtr (t, ats2) | C.TArray (t, _, ats2) -> CTRef (S.fresh S.Abstract, if CM.has_array_attr (ats1 @ ats2) then ISeq (0, CM.typ_width t) else IInt 0)
-      | _                                        -> halt <| C.bug "Unimplemented fresh_ctype: %a@!@!" C.d_type t
+      | _                                        -> halt <| C.bug "Unimplemented fresh_heaptype: %a@!@!" C.d_type t
