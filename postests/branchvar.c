6089377dfc5a4b5d530ed6fed010566c6a990668
Pat Rondon <pmr@plague>
2009-12-03 17:35:39 -0800

    Minimal test case for silly SSA bug

---
+++ postests/branchvar.c
@@ -0,0 +1,13 @@
+void main () {
+    switch (nondet()) {
+    case 1:
+        {
+            int s;
+            s = 0;
+        }
+        break;
+    case 2:
+        break;
+    default:
+    }
+}
