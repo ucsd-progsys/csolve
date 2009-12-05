8bce1ff6979941da74b5d33e56d238ce56f63877
Pat Rondon <pmr@famine>
2009-12-05 09:48:06 -0800

    demonstrate bug in strcpy

---
+++ negtests/strcpybug.c
@@ -0,0 +1,19 @@
+void test (const char *__attribute__((array)) src)
+{
+  char c;
+  c = *src;
+  // pmr: TOTALLY BOGUS
+  assert (c == 0);
+}
+
+void main () {
+    char s1[10];
+    int  off;
+
+    off = nondetpos();
+    if (off < 10) {
+        s1[off] = off;
+    }
+
+    test (s1);
+}
