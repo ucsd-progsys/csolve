56e5e3236f61e20eb301d024422196ed1e83e679
Ranjit Jhala <rjhala@pho.(none)>
2009-12-08 11:48:32 -0800

    eh? i just commited!

--- benchmarks/cyclone/cacm/c/adaptive_model.c
+++ benchmarks/cyclone/cacm/c/adaptive_model.c
@@ -10,7 +10,6 @@ int freq[No_of_symbols+1];	/* Symbol frequencies                       */
 start_model()
 {   int i;
     for (i = 0; i<No_of_chars; i++) {		/* Set up tables that       */
-        assert(i+1 <= 256);			//JHALA 
         char_to_index[i] = i+1;			/* translate between symbol */
         index_to_char[i+1] = i;			/* indexes and characters.  */
     }
@@ -36,19 +35,16 @@ update_model(symbol)
             cum += freq[i];
         }
     }
-    for (i = symbol; freq[i]==freq[i-1]; i--) ;	/* Find symbol's new index. */
-    int assm = assume(0 < i);			//JHALA
+    for (i = symbol; freq[i]==freq[i-1]; i--){ 	/* Find symbol's new index. */
+      int assm = assume(1 < i);			//JHALA: freq[0] != freq[1] 
+    }
     if (i<symbol) {
         int ch_i, ch_symbol;
         ch_i = index_to_char[i];		/* Update the translation   */
         ch_symbol = index_to_char[symbol];	/* tables if the symbol has */
         index_to_char[i] = ch_symbol;           /* moved.                   */
         index_to_char[symbol] = ch_i;
-        
-        assert(symbol <= 256);			//JHALA 
 	char_to_index[ch_i] = symbol;
-        
-        assert(i <= 256);			//JHALA 
 	char_to_index[ch_symbol] = i;
     }
     freq[i] += 1;				/* Increment the frequency  */
