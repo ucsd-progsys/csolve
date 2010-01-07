283bd73db853be6328a71ba45298952d92967e1b
Ranjit Jhala <rjhala@pho.ucsd.edu>
2010-01-06 17:36:44 -0800

    fiddling with the demo code: now generates a single JS file with all the annots.

--- demo/annotparse.py
+++ demo/annotparse.py
@@ -19,14 +19,16 @@
 # ON AN "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION
 # TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-import sys, string, shutil
+import sys, string, shutil, json
+
+template_js = "lookup.js"
 
 def make_vtab(src):
   vt = {} 
   f  = open(src, "r")
   for l in f:
     vs    = [t for t in l.split(" ") if t and not t.isspace()]
-    k     = (vs[0], vs[2])
+    k     = vs[0] + " at " + vs[2]
     d     = vs[3]
     vt[k] = d
   f.close()
@@ -59,5 +61,22 @@ def make_ttab(src):
       val = ""
   f.close()
   return tt
+
+def gen_jscript_string(base):
+  vt = make_vtab(base + ".vmap")
+  tt = make_ttab(base + ".annot")
+  js = "vt = eval(%s) \ntt = eval(%s) \n" 
+  js = js % (json.dumps(vt), json.dumps(tt))
+  return js
+
+def gen_jscript(base):
+  base_js = base  + ".js"
+  shutil.copy("lookup.js", base_js)
+  f       = open(base_js, "a")
+  tables  = gen_jscript_string(base)
+  f.write(tables)
+  f.close()
+
+
 
 
