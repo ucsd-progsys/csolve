ac513a44e9fbd58c88aa30a3454380a3324241a7
Pat Rondon <pmr@meatwad.ucsd.edu>
2010-02-08 10:55:20 -0800

    Add build and configure scripts to make initial build much easier

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 ########################## Fill in these fields
-OCAMLLIB=/home/rjhala/local/lib/ocaml/
+OCAMLLIB=/usr/lib/ocaml/3.10.2
 ##########################
 cd external/cil/; ./configure; make; make check; cd ../../
 cd external/ocamlgraph/; ./configure; make; make install; cd ../../
-cd external/z3/ocaml; ./build.sh $OCAMLLIB; cd ../../../
+cd external/z3/ocaml; ./build.sh /usr/lib/ocaml/3.10.2; cd ../../../
 cd src; make; cd ../
