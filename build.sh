084315309259c23a01ae14c9a35ca10a61e8a9f9
Pat Rondon <pmr@plague>
2010-02-08 12:08:06 -0800

    Kill build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-########################## Fill in these fields
-OCAMLLIB=/usr/lib/ocaml/3.10.2
-##########################
-cd external/cil/; ./configure; make; make check; cd ../../
-cd external/ocamlgraph/; ./configure; make; make install; cd ../../
-cd external/z3/ocaml; ./build.sh /usr/lib/ocaml/3.10.2; cd ../../../
-cd src; make; cd ../
