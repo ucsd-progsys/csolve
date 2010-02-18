370c421759ba1c5bd64dcabc1a09bee9ef1b4e81
Pat Rondon <pmr@famine>
2010-02-17 21:49:57 -0800

    Kill submodules, add script for fetching external modules, update README

---
+++ getsubmodules.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd external
+git clone meatwad.ucsd.edu:/git/external/z3
+git clone meatwad.ucsd.edu:/git/external/fixpoint
+git clone meatwad.ucsd.edu:/git/external/ocamlgraph
+git clone meatwad.ucsd.edu:/git/external/misc
+git clone meatwad.ucsd.edu:/git/external/cil
