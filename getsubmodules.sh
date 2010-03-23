7f34ab22686ab5f105b02512e9e3208ff3e58c25
Ranjit Jhala <jhala@chowder.ucsd.edu>
2010-03-23 10:27:52 -0700

    allow script to be parameterized by username

--- getsubmodules.sh
+++ getsubmodules.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
+if $1
+then
+  prefix="meatwad.ucsd.edu:/git/"
+else
+  prefix=$1"@meatwad.ucsd.edu:/git/"
+fi
+
+z3=$prefix"external/z3"
+fp=$prefix"external/fixpoint"
+og=$prefix"external/ocamlgraph"
+mi=$prefix"external/misc"
+cl=$prefix"external/cil"
+
+echo $z3
+echo $fp
+echo $og
+echo $mi
+echo $cl
+
 mkdir external
 cd external
-git clone meatwad.ucsd.edu:/git/external/z3
-git clone meatwad.ucsd.edu:/git/external/fixpoint
-git clone meatwad.ucsd.edu:/git/external/ocamlgraph
-git clone meatwad.ucsd.edu:/git/external/misc
-git clone meatwad.ucsd.edu:/git/external/cil
+git clone "$z3"
+git clone "$fp"
+git clone "$og"
+git clone "$mi"
+git clone "$cl"
