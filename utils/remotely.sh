99d3e2d2604b3a17a519555ec8549d69779134c6
Pat Rondon <prondon@cs.ucsd.edu>
2010-08-07 20:31:09 -0700

    script for running e.g. regrtest remotely

---
+++ utils/remotely.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -le 1 ]
+then
+    echo "Usage: $0 [HOST] [COMMAND]"
+    exit 1
+fi
+
+BASE=`dirname $0`
+WHERE=$1
+shift
+WHAT=$*
+
+cd $BASE/../
+CMD="LPATH=`mktemp -t`;
+mkdir \$LPATH;
+cd \$LPATH;
+tar zxf -;
+export LD_LIBRARY_PATH=\$LPATH/external/z3/lib/;
+$WHAT"
+tar czf - -X utils/package-exclude . | ssh $WHERE $CMD
