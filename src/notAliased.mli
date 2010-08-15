54b9486c16d1ee76ad795b20d3e1558715ee12e0
Pat Rondon <prondon@cs.ucsd.edu>
2010-08-15 13:47:44 -0700

    debugging printer

--- src/notAliased.mli
+++ src/notAliased.mli
@@ -1,3 +1,4 @@
 module NASet : Set.S with type elt = Sloc.t * Sloc.t
 
+val d_naset               : unit -> NASet.t -> Pretty.doc
 val non_aliased_locations : Ssa.cfgInfo -> ShapeInfra.shape -> NASet.t list array
