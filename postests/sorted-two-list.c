45a8bf4c6fdaeed942f08b077af967c0feb7f101
Pat Rondon <prondon@cs.ucsd.edu>
2010-08-29 16:20:02 -0700

    construct, verify a sorted list of two elements

---
+++ postests/sorted-two-list.c
@@ -0,0 +1,32 @@
+extern void *malloc (int);
+
+typedef struct _node {
+    int           val;
+    struct _node *next;
+} node;
+
+void test (node *hd) {
+    if (hd->next != (node *) 0) {
+        assert (hd->val < hd->next->val);
+    }
+
+    return;
+}
+
+int main () {
+    node *hd = (node *) malloc (sizeof (node));
+    hd->val  = nondet ();
+    hd->next = (node *) 0;
+
+    node *nxt = (node *) malloc (sizeof (node));
+    nxt->val  = nondet ();
+    nxt->next = (node *) 0;
+
+    if (hd->val < nxt->val) {
+        hd->next = nxt;
+    }
+
+    test (hd);
+
+    return 0;
+}
