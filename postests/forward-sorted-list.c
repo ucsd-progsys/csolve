aa118bd3089257fd4d774ab8254b74e469bf6636
Pat Rondon <prondon@cs.ucsd.edu>
2010-08-31 10:21:02 -0700

    sorted list constructed in order (rather than reverse)

---
+++ postests/forward-sorted-list.c
@@ -0,0 +1,36 @@
+extern void *malloc (int);
+
+typedef struct _node {
+    int           val;
+    struct _node *next;
+} node;
+
+void test_sorted (node *hd) {
+  node *cur = hd;
+
+  while (cur != (node *) 0 && cur->next != (node *) 0) {
+    assert (cur->val < cur->next->val);
+    cur = cur->next;
+  }
+
+  return;
+}
+
+int main () {
+  node *cur = (node *) malloc (sizeof (node));
+  cur->val  = 0;
+  cur->next = (node *) 0;
+
+  node *hd = cur;
+  for (int i = 1; i < 10; i++) {
+    node *n   = (node *) malloc (sizeof (node));
+    n->val    = i;
+    n->next   = (node *) 0;
+    cur->next = n;
+    cur       = n;
+  }
+
+  test_sorted (hd);
+
+  return 0;
+}
