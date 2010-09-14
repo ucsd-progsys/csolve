a404d6b6f14bcd59e972e8176e69446e88b62672
Pat Rondon <prondon@cs.ucsd.edu>
2010-09-14 15:57:34 -0700

    check sortedness of copied list in make-sorted-list

--- postests/make-sorted-list.c
+++ postests/make-sorted-list.c
@@ -16,8 +16,34 @@ void test_sorted (node *hd) {
     return;
 }
 
+// glib's s_list copy with noted modifications
+node *copy (node *hd) {
+    node *nnew = (node *) 0;
+
+    if (hd) {
+        node *last;
+
+        nnew       = (node *) malloc (sizeof (node));
+        nnew->val  = hd->val;
+        nnew->next = (node *) 0;
+        last       = nnew;
+        hd         = hd->next;
+        while (hd) {
+            // changed to kill in-place alloc (i.e., allocates to cur->next in glib)
+            node *cur  = (node *) malloc (sizeof (node));
+            cur->val   = hd->val;
+            cur->next  = (node *) 0;
+            last->next = cur;
+            last       = cur;
+            hd         = hd->next;
+        }
+    }
+
+    return nnew;
+}
+
 int main () {
-    node *cur = (node *) 0;
+    node *cur  = (node *) 0;
     node *prev = (node *) 0;
 
     for (int i = nondetnn (); i >= 0; i--) {
@@ -28,6 +54,7 @@ int main () {
     }
 
     test_sorted (cur);
+    test_sorted (copy (cur));
 
     return 0;
 }
