5522993dfc2399bb33f5451fbaf01455db2208bb
Pat Rondon <prondon@cs.ucsd.edu>
2010-10-11 11:30:27 -0700

    You asked for it: please use lcc instead of main.native from now on

--- src/liquidc.ml
+++ src/liquidc.ml
@@ -1,10 +1,7 @@
 (*
  *
- * Copyright (c) 2001-2002, 
- *  George C. Necula    <necula@cs.berkeley.edu>
- *  Scott McPeak        <smcpeak@cs.berkeley.edu>
- *  Wes Weimer          <weimer@cs.berkeley.edu>
- * All rights reserved.
+ * Copyright © 1990-2010 The Regents of the University of California.
+ * All rights reserved. 
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
