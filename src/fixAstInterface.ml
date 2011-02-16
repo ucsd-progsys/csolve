8bfecd8a2a0b2b866ed75e68b3fc45d86318ca8b
Ranjit Jhala <rjhala@ubuntu.(none)>
2011-02-16 15:24:02 -0800

    fixpoint builds

--- src/fixAstInterface.ml
+++ src/fixAstInterface.ml
@@ -134,7 +134,8 @@ let quals_of_file fname =
       |> open_in 
       |> Lexing.from_channel
       |> FixParse.defs FixLex.token
-      |> Misc.map_partial (function C.Qul p -> Some p | _ -> None) 
+      |> Misc.map_partial (function C.Qul p -> Some p | _ -> None)
+      |> Q.normalize
       >> Co.bprintf mydebug "Read Qualifiers: \n%a" (Misc.pprint_many true "" Q.print) 
   with Sys_error s ->
     Errormsg.warn "Error reading qualifiers: %s@!@!Continuing without qualifiers...@!@!" s; []
