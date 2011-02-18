d1b068a57618ef5783d5efe51b1fffc42fd94aaa
Ranjit Jhala <rjhala@ubuntu.(none)>
2011-02-17 19:29:07 -0800

    fixed duplicate qualifier name bug. next: move impgraph business closer to qual inst

--- src/fixAstInterface.ml
+++ src/fixAstInterface.ml
@@ -135,12 +135,8 @@ let quals_of_file fname =
       |> Lexing.from_channel
       |> FixParse.defs FixLex.token
       |> Misc.map_partial (function C.Qul p -> Some p | _ -> None)
-      |> Q.normalize
-      >> Co.bprintf mydebug "Read Qualifiers: \n%a" (Misc.pprint_many true "" Q.print) 
   with Sys_error s ->
     Errormsg.warn "Error reading qualifiers: %s@!@!Continuing without qualifiers...@!@!" s; []
-
-
 
 (* API *)
 let maybe_deref e =
