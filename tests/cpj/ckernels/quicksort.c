03b71214de6a0d2bbb3afcb022a5a070081e5961
Ranjit Jhala <rjhala@ubuntu.(none)>
2011-10-17 14:03:28 -0700

    NOW quicksort goes through

--- tests/cpj/ckernels/quicksort.c
+++ tests/cpj/ckernels/quicksort.c
@@ -1,34 +1,29 @@
 #include <cpj.h>
 #include <stdlib.h>
 
-//const int REF (V > 0) buf_len; 
-//const int REF (V > 0) too_small;
-
 const int REF (V > 0) buf_len = 10000; 
 const int REF (V > 0) too_small  = 50;
 
 
-//void swp(int * ARRAY a, int b, int c)
-//{
-//  int t = a[b];
-//  a[b] = a[c];
-//  a[c] = t;
-//}
-//
-//void seqsort(int * ARRAY a, int len){
-//  for (int i = 1; i < len; i++){
-//    int t = a[i];
-//    int j = i - 1;
-//    while (j >= 0 && a[j] > t){
-//      a[j + 1] = a[j];
-//      j--;
-//    }
-//    a[j + 1] = t;
-//  }
-//  return;
-//}
+void swp(int * ARRAY a, int b, int c)
+{
+  int t = a[b];
+  a[b] = a[c];
+  a[c] = t;
+}
 
-extern void qs(int * ARRAY VALIDPTR SIZE_GE(4*len) arr, int REF(V >= 0) len) OKEXTERN;
+void seqsort(int * ARRAY a, int len){
+  for (int i = 1; i < len; i++){
+    int t = a[i];
+    int j = i - 1;
+    while (j >= 0 && a[j] > t){
+      a[j + 1] = a[j];
+      j--;
+    }
+    a[j + 1] = t;
+  }
+  return;
+}
 
 //a: ptr(l, i) / l => (0+: int) -> len: int -> () / h: l => (0+; int)
 //                                                  r: l => T
@@ -38,58 +33,52 @@ void quicksort(int * ARRAY a, int len)
   int end = len - 1;
   int i, j, t;
 
-  int lt = nondetpos();
-  int tmp = lcc_assume(lt < len);
-  qs(a, lt + 1);
-  qs(a + lt + 1, len - lt);
-  return;
-}
+  //do a sequential sort if array becomes too small
+  if (len <= too_small) {
+    seqsort(a, len);
+    return;
+  }
 
-//
-//  //do a sequential sort if array becomes too small
-//  if (len <= too_small) {
-//    seqsort(a, len);
-//    return;
-//  }
-//
-//  int tmp = a[0];
-//  lcc_assert(len > 0);
-//
-//  //choose the median of the middle, beginning and end as the pivot
-//  //swap them into relative order while we're at it
-//  int mid = end / 2;
-//
-//  int lt = 1;
-//
-//  if (a[0] > a[mid])
-//    swp(a, 0, mid);
-//  if (a[mid] > a[end])
-//  {
-//    swp(a, mid, end);
-//    if (a[0] > a[mid])
-//      swp(a, 0, mid);
-//  }
-//
-//
-//  int lt = 1;
-//  int rt = end - 1;
-//  int pt = mid;
-//
-//  while(lt < rt) {
-//    while(lt < rt && a[pt] <  a[rt]) 
-//      rt--;
-//    while(lt < rt && a[lt] <= a[pt]) 
-//      lt++;
-//    if (lt < rt)
-//      swp(a, lt++, rt--);
-//  }
-//
-//  //cobegin
-//  //  rtn(quicksort(a, lt + 1))
-//  //  rtn(quicksort(a + lt + 1, len - lt))
-//  //coend
-//  
-//}
+  int tmp = a[0];
+  lcc_assert(len > 0);
+
+  //choose the median of the middle, beginning and end as the pivot
+  //swap them into relative order while we're at it
+  int mid = end / 2;
+
+  int lt = 1;
+
+  if (a[0] > a[mid])
+    swp(a, 0, mid);
+  if (a[mid] > a[end])
+  {
+    swp(a, mid, end);
+    if (a[0] > a[mid])
+      swp(a, 0, mid);
+  }
+
+
+  int lt = 1;
+  int rt = end - 1;
+  int pt = mid;
+
+  while(lt < rt) {
+    while(lt < rt && a[pt] <  a[rt]) 
+      rt--;
+    while(lt < rt && a[lt] <= a[pt]) 
+      lt++;
+    if (lt < rt)
+      swp(a, lt++, rt--);
+  }
+
+  lt++;
+
+  cobegin
+    rtn(quicksort(a, lt))
+    rtn(quicksort(a + lt, len - lt))
+  coend
+  
+}
 
 void initialize(int * ARRAY a, int len)
 { 
@@ -108,5 +97,4 @@ void main() // char ** argv, int argc)
   initialize(buf, len); 
   quicksort(buf, len);
 }
-
 
