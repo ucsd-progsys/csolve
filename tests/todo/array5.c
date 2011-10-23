1e0c0333bf1a763f4e90e0e2ef5743bb6262bb85
Alexander Bakst <abakst@cs.ucsd.edu>
2011-10-23 12:57:30 -0700

    Added array5.c as a testcase

---
+++ tests/todo/array5.c
@@ -0,0 +1,15 @@
+#include <liquidc.h>
+#include <stdlib.h>
+
+void process_array(int n)
+{
+  int ik = 0;
+  int r = 2;
+  int key[10];
+
+  while (r --> 0) {
+    key[ik++] = 0;
+    key[ik++] = 0;
+  }
+}
+
