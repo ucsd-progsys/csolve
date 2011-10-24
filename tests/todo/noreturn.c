dd9184a09e8ed354c45641c561e9144ae411782d
Pat Rondon <prondon@cs.ucsd.edu>
2011-10-24 09:27:19 -0700

    minimize noreturn

--- tests/todo/noreturn.c
+++ tests/todo/noreturn.c
@@ -1,14 +1,7 @@
 #include <stdlib.h>
 #include <liquidc.h>
 
-#define lcc_main(argc, argv)\
-  int \
-  main( int REF(V > 0) argc \
-      , char * STRINGPTR LOC(PROGRAM_NAME_LOC) * START NONNULL ARRAY SIZE(argc * 4) argv \
-      ) GLOBAL(PROGRAM_NAME_LOC) CHECK_TYPE
-
-// The following is marked unsafe without the return
-lcc_main (argc, argv) {
+int main(char ** START NONNULL ARRAY argv) CHECK_TYPE {
     exit (0);
     /* return 0; */
 }
