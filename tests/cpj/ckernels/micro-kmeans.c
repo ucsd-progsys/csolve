f1861b803751e7c9e2db8c46d7e70dda3d8b3d84
Pat Rondon <prondon@cs.ucsd.edu>
2011-10-27 17:47:33 -0700

    simplify further

--- tests/cpj/ckernels/micro-kmeans.c
+++ tests/cpj/ckernels/micro-kmeans.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 
 extern
-  float * START NONNULL SIZE(4 * sz2) ARRAY
+     int REF(V = sz2)
         * START NONNULL SIZE(4 * sz1) ARRAY
      mallocFloatMatrix (size_t REF(V > 0) sz1, size_t REF(V > 0) sz2)
   OKEXTERN;
 
 void
 normal_exec (int                              nfeatures,
-             float* ARRAY START * ARRAY START feature)
+             int * ARRAY START feature)
 {
     return;
 }
 
 void main(void)
 {
-  int nfeatures   = nondetpos();
-  int npoints     = nondetpos();
-  float** feature = mallocFloatMatrix(npoints, nfeatures);
+  int nfeatures = nondetpos();
+  int npoints   = nondetpos();
+  int* feature  = mallocFloatMatrix(npoints, nfeatures);
 
   normal_exec(nfeatures, feature);
 
