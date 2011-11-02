162046f29b8e419d104e01e1b06e7a3c08e8deee
Pat Rondon <prondon@cs.ucsd.edu>
2011-11-02 12:08:43 -0700

    reduce example with user effect

--- tests/cpj/examples/reduce.c
+++ tests/cpj/examples/reduce.c
@@ -1,4 +1,13 @@
 #include <cpj.h>
+#include <stdlib.h>
+
+LCC_EFFECT(EAccumulate)
+LCC_EFFECTS_COMMUTE(EAccumulate, EAccumulate)
+
+extern void accumulate (char * LOC(L) ARRAY l, int i)
+  EFFECT(L, && [V = l + i; EAccumulate = 1; ~(EWRITE = 1); ~(EREAD = 1)])
+  OKEXTERN
+;
 
 #define THREADS 8
 
@@ -23,24 +32,21 @@ int sum1 (char * ARRAY START NONNULL SIZE(len) a, int REF(V > 0) len)
   return a[0];
 }
 
-void sumStride (char * ARRAY a, int stride) {
+void sumStride (char * ARRAY a, int stride, char * ARRAY b) {
   foreach (i, 0, THREADS)
     for (int j = i; j < stride; j += THREADS) {
         a[j] = a[j + stride];
+        accumulate (b, j);
     }
-      /* int j = i; */
-      /* a[j] += a[j + stride]; */
   endfor
 }
 
 int sum2 (char * ARRAY START NONNULL SIZE(len) a, int REF(V > 0) len)
   CHECK_TYPE
 {
+  char *log = (char *) malloc (len);
   for (int stride = len / 2; stride > 0; stride /= 2)
-    sumStride (a, stride);
+    sumStride (a, stride, log);
 
   return a[0];
-}
-
-void main () {
 }
