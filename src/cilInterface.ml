ac9e66fcba4b7679d926ba3cf6019d3ac39b48c9
Pat Rondon <prondon@cs.ucsd.edu>
2011-12-26 20:15:50 -0800

    kill bot also for unimplemented operators

--- src/cilInterface.ml
+++ src/cilInterface.ml
@@ -183,7 +183,7 @@ and convert_cilbinexp (op, e1, e2) =
       let p1', p2' = Misc.map_pair pred_of_cilexp (e1, e2) in
       P (f [p1'; p2'])
   | Bunimpl ->
-      E (A.bot)
+      failwith "Unimplemented operator"
 
 
 (* API *)
