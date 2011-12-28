6e7af232244736cf348d05ce5e6334cd33561126
Pat Rondon <prondon@cs.ucsd.edu>
2011-12-27 18:23:47 -0800

    more prepass bug min, notes

--- tests/popl10-benchmarks/ft/ft_prepass_bug.c
+++ tests/popl10-benchmarks/ft/ft_prepass_bug.c
@@ -1,30 +1,28 @@
 #include <csolve.h>
 
 struct Vertices {
-   int key ;
+   int key;
 };
 
 struct Heap {
-   struct Vertices * LOC(IL) item ;
+   struct Vertices * LOC(IL) i;
 };
 
-int LessThan(struct Vertices * LOC(L) item1)
-{ 
-    return (item1->key < item1->key);
-}
-
 // If these are K, not L, it's ok - see note below
-// Shape mismatch? Inlining doesn't seem to work here...
-struct Heap INST(IL, IL) * LOC(L) Insert(struct Heap * LOC(L) h, struct Vertices * LOC(IL) i)
-{
-  LessThan(h->item);
-  return (0);
+struct Heap INST(IL, IL) * LOC(L) insert (struct Heap * LOC(L) h, struct Vertices * LOC(IL) i) {
+  int k = h->i->key;
+  return 0;
 }
 
 // w/ fresh location instead of L it's ok - is some capture happening here?
 // is there a location that's not closed in the spec?
 //   no - all specs are closed
-void MST(struct Vertices * LOC(L) graph) 
-{
-  Insert(0, graph);
+// loc shouldn't figure in to shape inference post-specgen!
+// why is this not fresh? or is it?
+// substs on locations should be disjoint but they wind up not being so because
+// of name reuse...?!
+// note new subst is simultaneous rather than extended,
+// but not getting treated like it...?
+void mst (struct Vertices * LOC(L) graph) {
+  insert (0, graph);
 }
