6e7fc80a21d08ca6d808332e406c429403f0a607
Pat Rondon <prondon@cs.ucsd.edu>
2012-01-18 14:23:45 -0800

    working under assumption there's always a comma

--- tests/cav12-demo/paper-demo.c
+++ tests/cav12-demo/paper-demo.c
@@ -2,9 +2,10 @@
 // Make IGNORE_INDEX the default on integers?
 // Another strategy: check moduli by index subtyping, check bounds by predicate typing
 // Different strategies for aliasing per-function?
+// Slow as hell - a lot of TRACKSTR going on, maybe it's to blame?
+// Try the whole program with driver and no annots
 
 #include <csolve.h>
-#include <string.h>
 #include <stdlib.h>
 
 // Typedef params inst automatically?
@@ -18,8 +19,9 @@ typedef struct _field_list {
   struct _field_list *         next;
 } INST(STRLOC, STRLOC) field_list;
 
-// Show unannotated version first, with code, then show extern decl
-char * LOC(L) NNREF(V >= s) NNREF(V <= s + n)
+// Show unannotated version first, with code, then show extern decl - in its own module
+// Also show only the quals we need to verify this first
+char * ARRAY LOC(L) NNREF(V >= s) NNREF(V < s + n) NNREF(PEQBLOCK(s))
   strnchr (char * STRINGPTR LOC(L) SIZE_GE(n) s, int NONNEG IGNORE_INDEX n, char c)
   CHECK_TYPE {
   for (; n-- && *s != '\0'; s++)
@@ -28,73 +30,38 @@ char * LOC(L) NNREF(V >= s) NNREF(V <= s + n)
   return NULL;
 }
 
-// Handle null case below
-/* field_list INST(STRLOC, L) *strnfields (char * STRINGPTR SIZE_GE(n) LOC(L) s, int NONNEG n) */
-/*   CHECK_TYPE */
-/* { */
-/*   field_list head; */
-/*   head.fld  = NULL; */
-/*   head.next = NULL; */
+// Also do something with the fields, like lowercase their contents
+field_list INST(STRLOC, L) *strnfields (char * STRINGPTR SIZE_GE(n) LOC(L) s, int NONNEG n)
+  CHECK_TYPE
+{
+  field_list head;
+  head.fld  = NULL;
+  head.next = NULL;
 
-/*   field_list *last = &head; */
-/*   while (n > 0) { */
-/*     char *spc = strnchr (s, n, ' '); */
+  field_list *last = &head;
+  while (n > 0) {
+    char *comma = strnchr (s, n, ',');
+    CSOLVE_ASSUME (comma != 0);
 
-/*     field *f = (field *) malloc (sizeof (field)); */
-/*     f->str = s; */
-/*     f->len = spc - s; */
+    field *f = (field *) malloc (sizeof (field));
+    f->str = s;
 
-/*     n -= f->len; */
+    if (comma) {
+      *comma = '\0';
+      f->len = comma - s;
+    } else {
+      f->len = n;
+    }
 
-/*     field_list *fl = (field_list *) malloc (sizeof (field_list)); */
-/*     fl->fld    = f; */
-/*     fl->next   = NULL; */
-/*     last->next = fl; */
-/*     last       = fl; */
-/*   } */
+    /* field_list *fl = (field_list *) malloc (sizeof (field_list)); */
+    /* fl->fld    = f; */
+    /* fl->next   = NULL; */
+    /* last->next = fl; */
+    /* last       = fl; */
 
-/*   return head.next; */
-/* } */
+    n -= f->len + 1;
+    s = comma + 1; // But note comma may be NULL
+  }
 
-// also uppercase tokens
-
-/* typedef struct _line { */
-/*     int                       len; */
-/*     char * ARRAY SIZE_GE(len) text; */
-/*     struct _line *            next; */
-/* } line; */
-
-/* line *filter (line *lines, char * ARRAY SIZE_GE(n) s, int POS n) */
-/*   CHECK_TYPE */
-/* { */
-/*     line first; */
-/*     first.len  = 0; */
-/*     first.text = 0; */
-/*     first.next = lines; */
-
-/*     // Idiomatic version, but not typable without final fields (is it */
-/*     // typable *with* them?) */
-/*     while (lines) { */
-/*         if (lines->next->len >= n && strncmp (lines->next->text, s, n) == 0) {  */
-/*             lines = lines->next; */
-/*         } else { */
-/*             lines->next = lines->next->next; */
-/*         } */
-/*     } */
-
-/*     /\* line *prev = &first; *\/ */
-/*     /\* line *curr = prev->next; *\/ */
-/*     /\* while (curr) { *\/ */
-/*     /\*     if (curr->len >= n && strncmp (curr->text, s, n) == 0) {  *\/ */
-/*     /\*         prev->next = curr; *\/ */
-/*     /\*         prev       = curr; *\/ */
-/*     /\*         curr       = curr->next; *\/ */
-/*     /\*     } else { *\/ */
-/*     /\*         curr = curr->next; *\/ */
-/*     /\*     } *\/ */
-/*     /\* } *\/ */
-
-/*     /\* prev->next = curr; *\/ */
-
-/*     return first.next; */
-/* } */
+  return head.next;
+}
