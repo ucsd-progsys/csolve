a361cbfe00f2d446aaf2ace718e96e828a7e1cba
Ranjit Jhala <jhala@tom-kha.local>
2012-02-11 18:31:24 -0800

    c2html now renders C using pygments, fwiw

--- demo/jquery/c2html.py
+++ demo/jquery/c2html.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python
-# Copyright (c) 2009 The Regents of the University of California. All rights reserved.
+# Copyright (c) 2009-12 The Regents of the University of California. All rights reserved.
 #
 # Permission is hereby granted, without written agreement and without
 # license or royalty fees, to use, copy, modify, and distribute this
@@ -24,17 +24,54 @@ from pygments import highlight
 from pygments.lexers import CLexer
 from pygments.formatters import HtmlFormatter
 
+def readFrom(file):
+  f  = open(infile, "r")
+  rv = f.read()
+  f.close()
+  return rv
+
+def writeTo(file, s):
+  f  = open(file, "w")
+  f.write(s)
+  f.close()
+
+def addLineNumber(i, line):
+  return ("<span class=\"line\" num=\"%d\">%s</span>" % (i, line))
+
+def isBlank(s):
+  return (len(s.split()) == 0)
+
+def numBlanks(s):
+  k = 0
+  for l in s.split("\n"):
+    if isBlank(l):
+      k += 1
+    else:
+      break
+  return k
+
+def addLineNumbers(src, html):
+  s    = html[28:-13] 
+  ins  = ["" for i in range(numBlanks(src))] + s.split("\n")
+  n    = len(ins)
+  outs = [addLineNumber(i, l) for (i, l) in zip(range(1, n+1), ins)]
+  return (html[:28] + "\n".join(outs) + html[-13:])
+
+def addCSS(html):
+  css = "<link rel=\"stylesheet\" type=\"text/css\" media=\"screen\" href=\"pyg_default.css\" />\n"
+  return (css + html)
+ 
+
 def main(infile, outfile):
-  code = open(infile, "r")
-  out  = open(outfile, "w")
-  highlight(code.read(), CLexer(), HtmlFormatter(), out)
-  code.close()
-  out.close()
+  src  = readFrom(infile)
+  html = highlight(src, CLexer(stripall=False), HtmlFormatter())
+  html = addLineNumbers(src, html)
+  html = addCSS(html)
+  writeTo(outfile, html)
 
 try:
   infile  = sys.argv[1]
   outfile = sys.argv[2]
-  print infile, outfile
- #main(infile, outfile)
+  main(infile, outfile)
 except:
   print "Usage: ./c2html.py <infile.c> <outfile.html>"
