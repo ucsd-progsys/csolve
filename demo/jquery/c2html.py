cda4f5bff4236173fb843879da9837b53dc73c34
Ranjit Jhala <jhala@tom-kha.local>
2012-02-12 13:18:54 -0800

    c2html now adds JSON to html file, and does error line highlighting

--- demo/jquery/c2html.py
+++ demo/jquery/c2html.py
@@ -25,7 +25,7 @@ from pygments.lexers import CLexer
 from pygments.formatters import HtmlFormatter
 from string import Template
 
-lineTplt = Template("<span class=\"line\" num=\"$linenum\"><span class=\"linenum\" num=\"$linenum\">$linenum:</span>$line</span>")
+lineTplt = Template("<span class=\"line\" num=$linenum><span class=\"linenum\" num=$linenum>$linenum:</span>$line</span>")
 srcTplt  = "templates/source.html"
 tgtTplt  = "templates/csolve.html" 
 tgtFile  = "csolve.html"
@@ -66,14 +66,21 @@ def addLineNumbers(src, html):
 
 ##################### Plugging Into Templates #########################
 
-def main(srcFile):
+def main(srcFile, jsonFile):
   src     = readFrom(srcFile)
   srcHtml = highlight(src, CLexer(stripall=False), HtmlFormatter())
   srcHtml = addLineNumbers(src, srcHtml)
-  writeTo(tgtFile, Template(readFrom(tgtTplt)).substitute(srcHtml = srcHtml)) 
+  srcJson = readFrom(jsonFile)
+  tplt    = Template(readFrom(tgtTplt))
+  tgt     = tplt.substitute(srcHtml = srcHtml, srcJson = srcJson)
+  writeTo(tgtFile, tgt)
 
 #############################################################################
 
-try: srcFile = sys.argv[1]
-except: print "Usage: ./c2html.py <infile.c>"
-main(sys.argv[1])
+try: 
+  srcFile  = sys.argv[1]
+  try: 
+    jsonFile = sys.argv[2]
+  except: jsonFile = srcFile + ".json"
+except: print "Usage: ./c2html.py <inFile.c> [inFile.json]"
+main(srcFile, jsonFile)
--- demo/jquery/csolve.js
+++ demo/jquery/csolve.js
@@ -8,16 +8,18 @@ var getVarLine = function(x){ return $(x).closest("span[class='line']").attr("nu
 var getVarInfo = function(x){ return (getVarName(x) + " at line: " + getVarLine(x)); }
 var toggleText = { 'Hide': 'Show', 'Show': 'Hide' };
 
+var hilitError = function(line){ 
+  if ($(line).attr("num") in csolveData.errorLines) {
+    $(line).addClass("errLine");
+  }
+}
+  
 $(document).ready(function(){
- 
+
   $("#showlines").click(function(){
     $("span[class='linenum']").slideToggle(); 
     $(this).text(toggleText[$(this).text()]);
   });
-
-  //$("span[class='line']").hover(yellowOn, yellowOff);
-  //prepend('DOG');
-  //"<span class=\"num\">" + $(this).attr("num") + ":</span>");
 
   //Hover-Highlights Variables and Functions
   $("span[class='n']").hover(yellowOn, yellowOff);
@@ -38,6 +40,10 @@ $(document).ready(function(){
     $("#msg").text("Function " + getVarInfo(this));
   });
 
+  //Color ErrorLines
+  $("span[class='line']").each(function(){ 
+    hilitError(this); 
+  });
 
 });
 
