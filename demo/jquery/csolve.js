84b370691af4a34b9ca4da003ae70cd3e78fdb7e
Ranjit Jhala <jhala@tom-kha.local>
2012-02-12 13:57:30 -0800

    temp commit

--- demo/jquery/csolve.js
+++ demo/jquery/csolve.js
@@ -1,19 +1,59 @@
-var redOn      = function(){$(this).addClass("red");};
-var redOff     = function(){$(this).removeClass("red");};
-var yellowOn   = function(){$(this).addClass("yellow");};
-var yellowOff  = function(){$(this).removeClass("yellow");};
+/****************************************************************/
+/************* Miscellaneous Globals ****************************/
+/****************************************************************/
 
-var getVarName = function(x){ return $(x).text();};
-var getVarLine = function(x){ return $(x).closest("span[class='line']").attr("num"); }
-var getVarInfo = function(x){ return (getVarName(x) + " at line: " + getVarLine(x)); }
 var toggleText = { 'Hide': 'Show', 'Show': 'Hide' };
 
+/****************************************************************/
+/************* CSS Helpers **************************************/
+/****************************************************************/
+
+var redOn      = function () { $(this).addClass("red"); };
+var redOff     = function () { $(this).removeClass("red"); };
+var yellowOn   = function () { $(this).addClass("yellow"); };
+var yellowOff  = function () { $(this).removeClass("yellow"); };
+
+/****************************************************************/
+/************* Accessing Selected Span Information **************/
+/****************************************************************/
+
+var getVarName = function(x){ 
+  return $(x).text();
+};
+
+var getVarLine = function(x){ 
+  return $(x).closest("span[class='line']").attr("num"); 
+};
+
+var getVarInfo = function(x){ 
+  return (getVarName(x) + " at line: " + getVarLine(x)); 
+};
+
 var hilitError = function(line){ 
-  if ($(line).attr("num") in csolveData.errorLines) {
+  if ($(line).attr("num") in csolveData.errorLines) 
+  {
     $(line).addClass("errLine");
-  }
-}
-  
+  };
+};
+
+/****************************************************************/
+/**************** Accessing csolveData Information **************/
+/****************************************************************/
+
+var annotVarLine = function(v, line) {
+  try { return csolveData.annot[v][line] }
+  catch(err) { return null };
+};
+
+var isErrorLine = function(i){
+  return (i in csolveData.errorLines);
+};
+
+/****************************************************************/
+/**************** Accessing csolveData Information **************/
+/****************************************************************/
+
+
 $(document).ready(function(){
 
   $("#showlines").click(function(){
