be9ae5d489e3fab06eddcd9ad2e835e83ae021ba
Pat Rondon <prondon@cs.ucsd.edu>
2012-03-17 20:22:07 -0700

    build updates

--- external/z3/ocaml/build-lib.sh
+++ external/z3/ocaml/build-lib.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export OCAMLLIB=$1
-
-gcc -I../include -I$OCAMLLIB -c z3_stubs.c -c z3_theory_stubs.c
+gcc -I../include -I`ocamlc -where` -c z3_stubs.c -c z3_theory_stubs.c
 ocamlopt -c z3.mli
 ocamlopt -c z3.ml
 ar rcs libz3stubs.a z3_stubs.o z3_theory_stubs.o
