f302125d346a620cec26fa704b86a01744d5f514
Alexander Bakst <abakst@cs.ucsd.edu>
2012-03-24 12:57:23 -0700

    made list2.c a little more complicated

--- tests/cpj/ckernels/list2.c
+++ tests/cpj/ckernels/list2.c
@@ -1,15 +1,20 @@
 #include <cpj.h>
 
-#define VALIDNODE START VALIDPTR ROOM_FOR(int)
+#define VALIDNODE START VALIDPTR ROOM_FOR(struct node)
 
-void main(int
+struct node {
+  int FINAL id;
+  int foo;
+};
+
+void main(struct node
    * REF((4*(DEREF([V]):int)) = (VVADDR - BLOCK_BEGIN([VVADDR]))) VALIDNODE
    * ARRAY START VALIDPTR SIZE_GE(4*sz) l,
    int REF(V > 0) sz)
 CHECK_TYPE
 {
   foreach(i, 0, sz)
-    int *n = l[i];
-    int b = *n;
+    struct node *n = l[i];
+    n->foo = 3;
   endfor
 }
