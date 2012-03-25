bf3298ad19ee9794fed2c1ed50fcff3297721691
Alexander Bakst <abakst@cs.ucsd.edu>
2012-03-24 20:53:55 -0700

    initial commit in this branch

--- tests/cpj/ckernels/unique_list.c
+++ tests/cpj/ckernels/unique_list.c
@@ -3,46 +3,46 @@
 #include <csolve.h>
 #include <stdlib.h>
 
+/* extern const int sz OKEXTERN; */
+
 typedef struct _list {
+  struct _list * NNVALIDPTR NNSTART next;
   int FINAL id;
-  struct _list * FINAL n;
+  //  int x;
 } list;
 
-struct _list;
-typedef struct _list list;
-
-
-list * alloc_node(int i)
+list* 
+alloc_node(int i)
 {
-  list * o = malloc(sizeof(list)); 
-  o->id    = i;
-  return o;
+ list* o = malloc(sizeof(list)); 
+ o->id = i;
+ 
+ return o;
 }
 
-void check_list(list * l, int sz) 
+int 
+main()//int argc, char ** argv) CHECK_TYPE
 {
-  list * c = l;
-  int i = 0;
-  for(i = 0, c = l; c != NULL, i < sz; c = c->n)
-    csolve_assert(c->id == i);
-}
+  int sz = 5;
+  if (sz <= 0)
+    return 0;
 
-int main(int argc, char ** argv)
-{
-  int sz = nondetpos() + 1;
-
-  list * root;
-  list * cur;
-  int i;
+  list *root, *cur;
+  int i = 1;
 
   root = alloc_node(0);
-  for(i = 1, cur = root; i < sz; i++, cur = cur->n)
-    cur->n = alloc_node(i); 
-    //cur :: { v: ptr(l1,.) | v.id = i}
-    //l1 -> (4: {v: ptr(l2,.) | v = NULL || v.id = i + 1 ^ v != vvaddr - 4})
-  cur->n = NULL;
-
-  check_list(root, sz);
-
-  return 0;
+  for(cur = root; i < sz; i++)
+  {
+    cur->next = alloc_node(i);
+    cur = cur->next;
+  }
+  
+  for(cur = root; cur->next != NULL; cur = cur->next);
+  {
+    if(cur)
+      validptr(cur->next);
+  }
+  /*   //cur :: { v: ptr(l1,.) | v.id = i} */
+  /*   //l1 -> (4: {v: ptr(l2,.) | v = NULL || v.id = i + 1 ^ v != vvaddr - 4}) */
+  
 }
