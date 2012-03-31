6ea03334ca35c6b7b6d2ae03ffb5316a763f9061
Ming Kawaguchi <mwookawa@cs.ucsd.edu>
2012-03-30 17:08:06 -0700

    listranking passes with super simple init

--- tests/cpj/ckernels/listranking.c
+++ tests/cpj/ckernels/listranking.c
@@ -30,20 +30,20 @@ void initRank(struct node * GOODNODE this, int i)
   this -> rankNbr = this -> next;
 }
 
-//void updateNbrRank(node * VALIDNODE this)
-//{
-//  this -> savedRank = this -> rank;
-//  this -> savedRankNext = this -> rankNbr;
-//}
-//  
-//void updateRank(node * VALIDNODE this)
-//{
-//  if (this -> rankNbr != this -> savedRankNext)
-//  {
-//    this -> rank += this -> savedRank;
-//    this -> rankNbr = this -> savedRankNext;
-//  }
-//}
+void updateNbrRank(struct node * GOODNODE this, int i)
+{
+  this -> savedRank = this -> rank;
+  this -> savedRankNext = this -> rankNbr;
+}
+  
+void updateRank(struct node * GOODNODE this, int i)
+{
+  if (this -> rankNbr != this -> savedRankNext)
+  {
+    this -> rank += this -> savedRank;
+    this -> rankNbr = this -> savedRankNext;
+  }
+}
 
 ////typedef node ** ARRAY list;
 //node * VALIDNODE LOC(L) 
@@ -68,30 +68,24 @@ void initRank(struct node * GOODNODE this, int i)
  
 void rank(struct node * GOODNODE * ARRAY START VALIDPTR SIZE_GE(4*sz) l,
           int REF(V > 0) sz)
-  CHECK_TYPE
 {
   //initialize the nodes for ranking
   foreach(i, 0, sz)
-  //for(int i = 0; i < sz; i++) {
     initRank(l[i], i);
-    //struct node * n = l[i];
-    //int b = n -> id;
-    //n -> rank = 5;
-  //}
   endfor
 
   //repeat log2(nodes.length) times
-//  int i = log2(sz);
-//  while (i-- > 0) {
-//    /* foreach(j, 0, sz) */
-//  for(int j = 0; j < sz; j++)
-//      updateNbrRank(l[j]);
-//    /* endfor */
-//    /* foreach(j, 0, sz) */
-//  for(int j = 0; j < sz; j++)
-//      updateRank(l[j]);
-//    /* endfor */
-//  }
+  int i = log2(sz);
+  while (i-- > 0) {
+  foreach(j, 0, sz)
+    //for(int j = 0; j < sz; j++)
+    updateNbrRank(l[j], j);
+  endfor
+  foreach(j, 0, sz)
+    //for(int j = 0; j < sz; j++)
+    updateRank(l[j], j);
+  endfor
+  }
 }
 
 ///* void swp(int * ARRAY START VALIDPTR SIZE_GE(4*(i+1)) SIZE_GE(4*(j+1)) a,  */
@@ -151,28 +145,20 @@ void rank(struct node * GOODNODE * ARRAY START VALIDPTR SIZE_GE(4*sz) l,
 /*     lst[j] -> next = lst[j]; */
 /* } */
 
-// extern
-//   void initialize(node * NNVALIDNODE
-//    * ARRAY NNSTART NNVALIDPTR SIZE_GE(4*sz) l,
-//    int //REF(&& [V >= 0; V < sz])
-//    * ARRAY //NNSTART NNVALIDPTR SIZE_GE(4*sz) idxs,
-//    idxs,
-//    int REF(V > 0) sz) OKEXTERN;
-
 ////AND THIS
 //extern
 //  int REF(&& [V >= 0; V < sz]) * START SIZE_GE(4*sz) VALIDPTR ARRAY
 //      initialize_idxs(int REF(V > 0) sz) OKEXTERN;
 
-//extern
-//  struct node * GOODNODE
-//  * START VALIDPTR SIZE_GE(4*sz) ARRAY 
-////  for some reason i can't get the right type out of initialize_idxs
-////      initialize_list(int REF(V >= 0) REF(V < sz) * START SIZE_GE(4*sz) VALIDPTR ARRAY idxs, int REF(V > 0) sz) OKEXTERN;
-//      initialize_list(int REF(V > 0) sz) OKEXTERN;
-//
-//
-//
+extern
+  struct node * GOODNODE
+  * START VALIDPTR SIZE_GE(4*sz) ARRAY 
+//  for some reason i can't get the right type out of initialize_idxs
+//      initialize_list(int REF(V >= 0) REF(V < sz) * START SIZE_GE(4*sz) VALIDPTR ARRAY idxs, int REF(V > 0) sz) OKEXTERN;
+      initialize_list(int REF(V > 0) sz) OKEXTERN;
+
+
+
 ///* void runTest(node ** ARRAY l, int * ARRAY idxs, int sz) */
 ///* { */
 ///*   /\* int i; *\/ */
@@ -181,48 +167,48 @@ void rank(struct node * GOODNODE * ARRAY START VALIDPTR SIZE_GE(4*sz) l,
 ///* } */
 //
 // 
-//void runWork(struct node * GOODNODE * ARRAY START VALIDPTR SIZE_GE(4*sz) l, 
-//             int REF(V > 0) sz)
-//{
-//  rank(l, sz);
-//}
-//
-//
-///* MK NOTES
-// * 1) how much of initialization can we _not_ extern?
-// *     -quite a bit actually
-// * 2) can we do without the ghost field? (ie, is the array index always in scope?)
-// *     -only if we have another way of attaching the array index to the node ptr
-// * */
-// 
-//int main(char ** argv, int argc)
-//  CHECK_TYPE
-//{
-//  int sz;
-//  struct node   *l;
-////  int        *idxs;
-////  int arrr;
-//
-//   if (argc < 1 || argc > 4)
-//     return 1;
-//
-//  if (argc >= 3)
-//    sz = nondetpos(); //atoi(argv[2]);
-//  else
-//    exit(1);
-//
-//  if (sz <= 0) return 0;
-//
-//  //LOOK AT THIS PAT
-////  idxs = initialize_idxs(sz);
-////  arrr = idxs[0];
-//// csolve_assert(arrr < sz);
-//
-////  l    = initialize_list(idxs, sz);
-//  l    = initialize_list(sz);
-//  //runTest(*l, *idxs, sz);  
-//  runWork(l, sz);
-//  
-//  return 0;
-//}
-//
+void runWork(struct node * GOODNODE * ARRAY START VALIDPTR SIZE_GE(4*sz) l, 
+             int REF(V > 0) sz)
+{
+  rank(l, sz);
+}
+
+
+/* MK NOTES
+ * 1) how much of initialization can we _not_ extern?
+ *     -quite a bit actually
+ * 2) can we do without the ghost field? (ie, is the array index always in scope?)
+ *     -only if we have another way of attaching the array index to the node ptr
+ * */
+ 
+int main(char ** argv, int argc)
+  CHECK_TYPE
+{
+  int sz;
+  struct node   *l;
+//  int        *idxs;
+//  int arrr;
+
+   if (argc < 1 || argc > 4)
+     return 1;
+
+  if (argc >= 3)
+    sz = nondetpos(); //atoi(argv[2]);
+  else
+    exit(1);
+
+  if (sz <= 0) return 0;
+
+  //LOOK AT THIS PAT
+//  idxs = initialize_idxs(sz);
+//  arrr = idxs[0];
+// csolve_assert(arrr < sz);
+
+//  l    = initialize_list(idxs, sz);
+  l    = initialize_list(sz);
+  //runTest(*l, *idxs, sz);  
+  runWork(l, sz);
+  
+  return 0;
+}
+
