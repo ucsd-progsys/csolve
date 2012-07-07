df85fbf30db905e01b4674f0fda92d0d560469ce
Pat Rondon <prondon@cs.ucsd.edu>
2012-07-07 12:25:05 -0700

    use editor in demo

--- demo/index.php
+++ demo/index.php
@@ -51,6 +51,15 @@ function writeTextFile($fname,$fld){
   fclose($f);
 }
 
+function getFieldOrFile ($entered_program, $field, $filename) {
+  if($entered_program == 1) {
+    return (htmlspecialchars (getRawTextFromField ($field)));
+  }
+  else {
+    return (htmlspecialchars (file_get_contents ($filename)));
+  }
+}
+
   if($_POST['chooseform'] == "1") {
     $demo = $category[$_POST['choosedemo']]; 
   }
@@ -82,6 +91,13 @@ function writeTextFile($fname,$fld){
   <script src="http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js"></script>
   <script src="js/jquery.ba-hashchange.min.js"></script>
   <script src="js/csolve.js"></script>
+  <style type="text/css">
+    .hidden {
+      visibility: hidden;
+      height: 0;
+      width: 0;
+    }
+  </style>
 </head>
 <body>
   <h1>CSolve Demo</h1>
@@ -106,35 +122,41 @@ function writeTextFile($fname,$fld){
 </select>
 </form>
 
-<form action='<? echo $_SERVER['PHP_SELF']; ?>' 
-      method='post'><p>
+<script>
+  function setTextAreaValue (id, editor) {
+    document.getElementById (id).value = editor.getSession ().getValue ();
+  }
+</script>
 
-<h3>Logical Qualifiers</h3>
-<textarea name='qualifiers' rows='5' cols='80'>
-<?
-  if($entered_program == 1) {
-    echo (getRawTextFromField('qualifiers'));
-  }
-  else {
-    echo (file_get_contents($demo.".c.hquals"));
-  }
-?>
-</textarea>
+<form action='<? echo $_SERVER['PHP_SELF']; ?>' 
+      method='post'
+      onSubmit='setTextAreaValue ("program", editor); setTextAreaValue ("qualifiers", qualEditor)'><p>
+
+<h3>Predicate Templates</h3>
+
+<div id="qualifiers-editor" style="position: relative; width: 60em; height: 10em"><?
+  echo (getFieldOrFile ($entered_program, 'qualifiers', $demo.".c.hquals"));
+?></div>
+<textarea id='qualifiers' name='qualifiers' class="hidden"></textarea>
 
 <h3>C Program</h3>
 
-<textarea name='program' rows='20' cols='80'>
-<?
-  if($entered_program == 1) {
-    echo (getRawTextFromField('program'));
-  }
-  else {
-    echo (file_get_contents($demo.".c"));
-  }
-?>
-</textarea>
+<div id="program-editor" style="position: relative; width: 60em; height: 30em"><?
+  echo (getFieldOrFile ($entered_program, 'program', $demo.".c"));
+?></div>
+<textarea id='program' name='program' class="hidden"></textarea>
+
+<script src="ace/ace.js" type="text/javascript" charset="utf-8"></script>
+<script src="ace/mode-c_cpp.js" type="text/javascript" charset="utf-8"></script>
+<script>
+  var qualEditor = ace.edit ("qualifiers-editor");
+  var editor = ace.edit ("program-editor");
+  var CMode = require ("ace/mode/c_cpp").Mode;
+  editor.getSession ().setMode (new CMode ());
+</script>
 
 <br />
+
 <input name='programform' type='hidden' value='1'>
 <input type='submit' value='csolve'>
 </p></form>
